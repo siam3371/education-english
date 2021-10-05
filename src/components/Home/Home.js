e5b1a8b21b43ddc401ed911345108cff42884bd5
siam3371 <siamhossain3371@gmail.com>
2021-10-05 23:36:19 +0600

    git push

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,29 +6,29 @@ import Teachers from '../Teachers/Teachers';
 
 const Home = () => {
     const [teachers, setTeachers] = useState([]);
-    useEffect(()=> {
+    useEffect(() => {
         fetch('./teachers.JSON')
-        .then(res => res.json())
-        .then(data => setTeachers(data))
-    },[]) 
+            .then(res => res.json())
+            .then(data => setTeachers(data))
+    }, [])
     return (
-       <div className="container">
+        <div className="container">
             <h1 className="text-center m-5 text-primary"> Meet our Engslish Educational teacher Services</h1>
-             <div className="row"> 
-             <div className="col-lg-4 col-md-4 hover-overlay shadow-lg p-3 text-center m-5 bg-body rounded"> 
-             <h1 className="mb-5">Teacher Services </h1> 
-             {
-                     teachers.map(teacher => <Teachers
-                        key={teacher.id}
-                        teacher ={teacher}></Teachers>)
-                 } 
-                  </div>
-                 <div className="col-lg-6 col-md-6">
-               <h3>Popular English Books</h3>    
-               <Books></Books>
-               </div>  
-              </div>
-       </div>
+            <div className="row">
+                <div className="col-lg-4 col-md-4 hover-overlay shadow-lg p-3 text-center m-5 bg-body rounded">
+                    <h1 className="mb-5">Teacher Services </h1>
+                    {
+                        teachers.map(teacher => <Teachers
+                            key={teacher.id}
+                            teacher={teacher}></Teachers>)
+                    }
+                </div>
+                <div className="col-lg-6 col-md-6">
+                    <h3>Popular English Books</h3>
+                    <Books></Books>
+                </div>
+            </div>
+        </div>
     );
 };
 
